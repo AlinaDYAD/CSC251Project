0d982e577fbc99b5b963ffacf6aae44d04716820
Alina <alin06ko@gmail.com>
2024-11-05 19:08:57 -0500

    Modified to read input from a file

--- Project_alina_dyadchenko.java
+++ Project_alina_dyadchenko.java
@@ -1,9 +1,69 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Project_alina_dyadchenko 
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws FileNotFoundException
    {
+      //create an ArrrayList to store Policy objects
+      ArrayList<Policy> policies = new ArrayList<>();
+      
+      //open the file
+      File file = new File("PolicyInformation.txt");
+      
+      //Ensure file exists and read each policy
+      if (file.exists())
+      {
+         Scanner fileScanner = new Scanner(file); //"open"
+         
+         while (fileScanner.hasNext())
+         {
+            int policyNumber = fileScanner.nextInt();
+            fileScanner.nextLine(); // consume the newline
+            
+            String providerName = fileScanner.nextLine();
+            String firstName = fileScanner.nextLine();
+            String lastName = fileScanner.nextLine();
+            int age = fileScanner.nextInt();
+            fileScanner.nextLine(); // consume the newline after the int
+            
+            String smokingStatus = fileScanner.nextLine();
+            double height = fileScanner.nextDouble();
+            double weight = fileScanner.nextDouble();
+            
+            fileScanner.nextLine(); //clear the newline
+            
+            //Create a Policy object with the read values
+            Policy policy = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
+            
+            //Add the Polucy object to the ArrayList
+            policies.add(policy);
+         }
+         fileScanner.close();
+         
+         //Display the policy information and calculated results
+         for (Policy policy : policies)
+         {
+            System.out.println("\nPolicy Number: " + policy.getPolicyNumber());
+            System.out.println("Provider Name: " + policy.getTheName());
+            System.out.println("Policyholder's Fist Name: " + policy.getFirstName());
+            System.out.println("Policyholder's Last Name: " + policy.getLastName());
+            System.out.println("Policyholder's Age: " + policy.getTheAge());
+            System.out.println("Policyholder's Smoking Status: " + policy.getSmokingStatus());
+            System.out.println("Policyholder's Height: " + policy.getHeight());
+            System.out.println("Policyholder's Weight: " + policy.getWeight());
+            System.out.printf("Policyholder's BMI: %.2f\n", policy.calculateBMI());
+            System.out.printf("Policy Price: $%.2f\n", policy.calculatePolicyPrice());
+         }
+
+      } else
+        {
+         System.out.println("File 'PolicyInformation.txt' does not exist. Please ensrue the file is available.");
+        }
+      
+      /**
       //Crete a Scanner object for user input
       Scanner keyboard = new Scanner(System.in);
       
@@ -38,6 +98,8 @@ public class Project_alina_dyadchenko
       //Create a Policy object with user-provided values
       Policy policy = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
       
+      
+      
       //Display the policy information and calculated results
       System.out.println("\nPolicy Number: " + policy.getPolicyNumber());
       System.out.println("Provider Name: " + policy.getTheName());
@@ -52,5 +114,7 @@ public class Project_alina_dyadchenko
 
       //Close the scanner
       keyboard.close();
+      
+      */
    }
 }
